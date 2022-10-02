c0a0f364454c94e1e8290c9dedd95eedf7f75ede
M Bilal <mohammadbilal200@hotmail.com>
2022-10-02 09:55:24 +0500

    initial commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function login(){
+    document.getElementById("main-container1").style.display="none";
+    document.getElementById("main-container2").style.display="block";
+}
+function homePage(){
+    document.getElementById("main-container2").style.display="none";
+    document.getElementById("main-container1").style.display="block";
+}
